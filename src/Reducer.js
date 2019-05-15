65057c063c6b442bee71c6f1c113e0615b580b08
thoaiky1992 <thoaiky1992@gmail.com>
2019-05-15 13:48:22 +0700

    Revert "Revert "chuyển đổi mã môn học thành tên môn học""
    This reverts commit 226440ad09a4d9d690a5a388e19b2b0579357d63.

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,16 +1,18 @@
-
-
 var DefaultState  = {
     isLogin : false,
     ds_giang_day : [],
     mang_ca : [],
     ca : 0,
-    danh_sach_sinh_vien : []
+    danh_sach_sinh_vien : [],
+    ds_mon_hoc : []
     
 };
 
 const Reducer = (state = DefaultState ,  action) => {
     switch (action.type) {
+        case 'GET_ALL_DS_TEN_MON_HOC':
+            state.ds_mon_hoc = action.data;
+            return {...state};
         case 'CHOOSE_CA_AND_GET_DS' : 
             state.ca = action.data.ca;
             return {...state,danh_sach_sinh_vien : action.data.danh_sach_sinh_vien};
